147296f59a7d72562a6a88e7665fe40458ba3d5d
hunterowens <owens.hunter@gmail.com>
2015-05-07 22:32:12 -0500

    some work

--- baseballParser.py
+++ baseballParser.py
@@ -6,3 +6,23 @@ r = requests.get(url)
 data = r.json()
 games = data['data']['games']['game']
 
+last_game = games.pop()
+
+def findGame():
+    """
+    finds the game out of the game list, returns it
+    """
+    return last_game
+
+game = findGame()
+
+def runners():
+    """
+    Turns on the light for the runners
+    """
+    pass
+
+def atBat():
+    pass
+
+
