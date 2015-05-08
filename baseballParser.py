2592ffe3cc9b0f4fa8e88623fdab2bae1517c4eb
hunterowens <owens.hunter@gmail.com>
2015-05-07 19:17:25 -0500

    mlb json is weird

--- baseballParser.py
+++ baseballParser.py
@@ -3,4 +3,6 @@ import requests
 url = "http://gd2.mlb.com/components/game/mlb/year_2015/month_05/day_07/master_scoreboard.json"
 
 r = requests.get(url)
-date = r.json()
+data = r.json()
+games = data['data']['games']['game']
+
